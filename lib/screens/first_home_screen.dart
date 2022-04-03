d5a828122875e45185c3114599c7d26cc36842ed
aboudalaeddine <aboudalaeddine@gmail.com>
2022-04-03 10:37:24 +0000

    modification

--- lib/screens/first_home_screen.dart
+++ lib/screens/first_home_screen.dart
@@ -30,7 +30,6 @@ class _FirstHomeScreenState extends State<FirstHomeScreen> {
                     padding: EdgeInsets.fromLTRB(
                         20, MediaQuery.of(context).size.height * 0.2, 20, 0),
                     child: Column(children: <Widget>[
-                      UserTypeButton(context, "ekfj", () {}),
                       UserTypeButton(context, "PARENT", () {}),
                       UserTypeButton(context, "DIRECTEUR", () {}),
                       UserTypeButton(context, "INSPECTEUR", () {}),
